39db34a4dc8691e7ede5df1a30f183bb0e773892
Catalin Boruga <catalinboruga@yahoo.com>
2016-04-12 20:46:34 +0200

    Finish posts scheduled

---
+++ app/models/post.rb
@@ -0,0 +1,18 @@
+class Post < ActiveRecord::Base
+  scope :draft, ->{ where(published_at: nil)}
+  scope :published, ->{ where.not(published_at: nil).where("published_at <= ?", Time.zone.now)}
+  scope :scheduled, ->{ where.not(published_at: nil).where("published_at > ?", Time.zone.now)}
+
+  attr_accessor :status
+
+  def clean_up_status
+    self.published_at = case status
+                        when "Draft"
+                          nil
+                        when "Published"
+                          Time.zone.now
+                        else
+                          published_at  
+                        end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160412171805) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "body"
+    t.datetime "published_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "title"
+  end
 
 end
